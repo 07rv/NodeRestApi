e946e830746224c1644ddceaa36430d04573be88
Rohit verma <28vrohit@gmail.com>
2022-09-24 16:57:56 +0530

    .

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const morgan = require("morgan")
 
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 dotenv.config()
 
@@ -29,6 +30,7 @@ app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(3000, () => {
     console.log("hi...")
